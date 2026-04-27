85f5efd2de6df8886c0f04ca0696c663a60dfdf8
Halil Ismail <ismailhalil073@gmail.com>
2026-04-27 16:34:11 +0300

    finish the home Page

--- src/components/LatestNews/latestNews.jsx
+++ src/components/LatestNews/latestNews.jsx
@@ -1,7 +1,14 @@
 import test from "/images/test.jpg"
 function LatestNews() {
-    return <section className="w-[331px] h-[303px] bg-[#1C1C1C] " >
-<img src={test} alt="" />
+    return <section className=" relative w-[98%] mx-auto md:w-[331px] h-[303px] bg-[#1C1C1C] flex items-center justify-center rounded-[12px] " >
+        <img src={test} alt="" className="w-[90%] h-[90%] rounded-[12px] object-cover" />
+        <div className="absolute w-[88%] left-1/2 -translate-x-[50%] bottom-4 ">
+            <h5 className="text-[#D2FF00] italic text-[24px] font-['Platypi'] " >A New talent has joined</h5>
+            <p className=" text-white font-['Chakra_Petch'] " >OA Sports is a multifaceted sports
+                consulting and management company
+                that brings together multidisciplinary
+                expertise under one roof.</p>
+        </div>
     </section>
 }
 export default LatestNews
--- src/page/Home/home.jsx
+++ src/page/Home/home.jsx
@@ -132,7 +132,7 @@ function Home() {
 
 
             <div className="relative hidden lg:block ">
-                <div className="absolute z-0 left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] w-[1071px] h-[50%] rounded-full bg-[#d0ff002a] blur-[400px]"></div>
+                <div className="absolute z-0 left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] xl:w-[1071px] h-[50%] rounded-full bg-[#d0ff002a] blur-[400px]"></div>
                 <div className="relative z-20 flex flex-col gap-6 py-[48px] px-[96px]">
                     <div className="flex items-center justify-between w-[90%] mx-auto">
                         <h4 className="text-white font-['Platypi'] font-bold italic text-[32px] leading-[100%]">
@@ -146,9 +146,9 @@ function Home() {
 
                     <div className="flex gap-6 justify-evenly w-[90%] mx-auto">
                         <div className="flex flex-col gap-4">
-                            <div className="w-[720px] h-[313px] rounded-[32px] bg-[#1C1C1C] p-3 flex">
+                            <div className="w-[100%] min-h-[313px] rounded-[32px] bg-[#1C1C1C] p-3 flex flex-col  xl:flex-row gap-2">
                                 <div>
-                                    <h5 className="font-bold font-['Platypi'] italic text-[26px] text-[#D2FF00]">
+                                    <h5 className="font-bold font-['Platypi'] italic text-[26px] text-[#D2FF00]  ">
                                         test
                                     </h5>
 
@@ -160,13 +160,13 @@ function Home() {
                                 </div>
 
                                 <img
-                                    className="min-w-[307px] h-[289px] object-cover rounded-[20px]"
+                                    className="w-full xl:w-[90%] mx-auto h-[289px] object-cover rounded-[20px]"
                                     src={test}
                                     alt="test"
                                 />
                             </div>
 
-                            <div className="w-[713px] h-[836px] rounded-[32px] p-3 bg-[#1C1C1C] flex flex-col justify-between">
+                            <div className="w-full h-[836px] xl:min-h-[836px] rounded-[32px] p-3 bg-[#1C1C1C] flex flex-col gap-4 justify-between">
                                 <div>
                                     <h5 className="font-bold font-['Platypi'] italic text-[26px] text-[#D2FF00]">
                                         test
@@ -180,7 +180,7 @@ function Home() {
                                 </div>
 
                                 <img
-                                    className="min-w-full h-[80%] object-cover rounded-[20px]"
+                                    className="w-full h-[80%] object-cover rounded-[20px]"
                                     src={test}
                                     alt="test"
                                 />
@@ -188,8 +188,8 @@ function Home() {
                         </div>
 
                         <div className="flex flex-col gap-6">
-                            <div className="w-[506px] h-[672px] rounded-[32px] bg-[#1C1C1C] p-3 flex flex-col justify-between gap-3">
-                                <div>
+                            <div className="w-full h-[672px] xl:min-h-[672px] rounded-[32px] bg-[#1C1C1C] p-3 flex flex-col justify-between gap-3">
+                                <div >
                                     <h5 className="font-bold font-['Platypi'] italic text-[26px] text-[#D2FF00]">
                                         test
                                     </h5>
@@ -202,13 +202,13 @@ function Home() {
                                 </div>
 
                                 <img
-                                    className="min-w-full h-[74%] object-cover rounded-[20px]"
+                                    className="w-full h-[74%] object-cover rounded-[20px]"
                                     src={test}
                                     alt="test"
                                 />
                             </div>
 
-                            <div className="bg-[#1C1C1C] p-3 w-[506px] h-[439px] rounded-[32px] flex flex-col gap-2 justify-between">
+                            <div className="bg-[#1C1C1C] p-3 w-full h-[439px] xl:min-h-[539px] rounded-[32px] flex flex-col gap-2 justify-between">
                                 <div>
                                     <h5 className="font-bold font-['Platypi'] italic text-[26px] text-[#D2FF00]">
                                         test
@@ -222,7 +222,7 @@ function Home() {
                                 </div>
 
                                 <img
-                                    className="min-w-full h-[60%] object-cover rounded-[20px]"
+                                    className="ull h-[60%] xl:h-[70%] object-cover rounded-[20px]"
                                     src={test}
                                     alt="test"
                                 />
@@ -231,35 +231,41 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <LatestNews/>
-                {/*  
+            <div className="py-10 flex lg:hidden flex-wrap gap-4 ">
+                <LatestNews />
+                <LatestNews />
+                <LatestNews />
+                <LatestNews />
+                <LatestNews />
+                <LatestNews />
+            </div>
 
-            <div className="w-[80%] min-h-[500px] pb-52 mx-auto">
-                <h2 className="font-bold font-['Platypi'] text-[46px] leading-[100%] italic text-white ">Our <span className=" text-[#D2FF00] " >Partnership</span></h2>
-                <div className="flex flex-col gap-8" >
+            <div className="w-[80%] h-[50%] lg:min-h-[500px] pb-20 md:pb-52 mx-auto gap-8 flex flex-col md:gap-0 ">
+                <h2 className="font-bold font-['Platypi'] text-[30px] md:text-[46px] leading-[100%] italic text-white ">Our <span className=" text-[#D2FF00] " >Partnership</span></h2>
+                <div className="flex flex-col gap-10 md:gap-8" >
                     <div className="flex items-center justify-center gap-2 ">
-                        <img className="w-[150px]" src={Logo} alt="logo" />
-                        <img className="w-[150px]" src={Sbu} alt="sbu" />
+                        <img className="w-[121px]  md:w-[150px]" src={Logo} alt="logo" />
+                        <img className="w-[121px] md:w-[150px]" src={Sbu} alt="sbu" />
                     </div>
-                    <div className="w-full h-[490px] flex justify-between items-center rounded-[32px] p-[28px] bg-[#1C1C1C] ">
+                    <div className="w-full h-[20%] lg:min-h-[500px] xl:h-[490px] gap-8 flex flex-col-reverse lg:flex-row justify-between items-center rounded-[32px] md:p-[28px] md:bg-[#1C1C1C] ">
                         <div className="flex flex-col gap-8 justify-evenly h-full " >
-                            <h2 className=" text-[48px] leading-[100%] font-['Platypi'] text-white italic font-bold ">Partners with <span className="text-[#D2FF00]" >SBU university !</span></h2>
+                            <h2 className=" text-[26px] md:text-[48px] leading-[100%] font-['Platypi'] text-white italic font-bold ">Partners with <span className="text-[#D2FF00]" >SBU university !</span></h2>
                             <div className="flex flex-col gap-8 " >
-                                <p className="text-white text-[24px] font-['Chakra_Petch'] leading-[100%]">OA Sports is a multifaceted sports consulting and management company
+                                <p className="text-white md:text-[24px] font-['Chakra_Petch'] leading-[100%]">OA Sports is a multifaceted sports consulting and management company
                                     that brings together multidisciplinary expertise under one roof, OA Sports
                                     is a multifaceted sports consulting and management company that brings
                                     together multidisciplinary expertise under one roof.</p>
-                                <p className="text-white text-[24px] font-['Chakra_Petch'] leading-[100%]" >OA Sports is a multifaceted sports consulting and management company
+                                <p className="text-white md:text-[24px] font-['Chakra_Petch'] leading-[100%]" >OA Sports is a multifaceted sports consulting and management company
                                     that brings together multidisciplinary expertise under one roof.</p>
                             </div>
                             <div className="cursor-pointer py-[18px] px-6 rounded-[8px] bg-[#D2FF00] flex items-center w-[280px] justify-center shadow-[0_8px_16px_0_#D2FF0029,0_40px_24px_0_#D2FF0021,0_18px_18px_0_#D2FF0036,0_4px_10px_0_#D2FF0040] ">
                                 <p className="font-bold text-[20px] font-['Chakra_Petch'] leading-[100%]  ">Pricing Plans</p>
                             </div>
                         </div>
-                        <img className="h-full rounded-[24px]" src={test} alt="test" />
+                        <img className="h-[80%] md:h-[450px] lg:w-full xl:h-full rounded-[24px] object-cover" src={test} alt="test" />
                     </div>
                 </div>
-            </div> */}
+            </div>
         </section>
     );
 }
